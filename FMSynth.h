da9cd77eca5ba63a832a15404b0ff6cd58fad88d
jpfli <46481986+jpfli@users.noreply.github.com>
2020-09-18 22:10:36 +0300

    Added didgeridoo

--- FMSynth.h
+++ FMSynth.h
@@ -124,7 +124,7 @@ class FMSynth {
                     }
                 }
             }, { // Bank 2
-                .num_patches=12,
+                .num_patches=13,
                 .patches={
                     {
                         "PIANO",
@@ -224,6 +224,16 @@ class FMSynth {
                             {.level=100, .pitch={.fixed=false, .coarse=1, .fine=0}, .detune=50, .attack=0, .decay=0, .sustain=100, .loop=false}, 
                             {.level=20, .pitch={.fixed=false, .coarse=1, .fine=0}, .detune=51, .attack=0, .decay=0, .sustain=100, .loop=false}, 
                             {.level=0, .pitch={.fixed=false, .coarse=1, .fine=0}, .detune=50, .attack=0, .decay=0, .sustain=100, .loop=false}, 
+                            {.level=0, .pitch={.fixed=false, .coarse=1, .fine=0}, .detune=50, .attack=0, .decay=0, .sustain=100, .loop=false}
+                        }
+                    }, {
+                        "DIDGERIDOO E2", // 44 49 44 47 45 52 49 44 4F 4F 20 45 32 00 00 00 02 50 32 28 23 00 64 37 00 00 00 64 01 09 1D 32 00 00 64 00 19 01 0A 5D 32 00 37 4B 00 32 00 00 32 32 32 00 64 00 00 00 01 00 32 00 00 64 00
+                        .algorithm=2, .volume=80, .feedback=50, .glide=40, .attack=35, .decay=0, .sustain=100, .release=55, 
+                        .lfo={.speed=0, .attack=0, .pmd=0}, 
+                        .op={
+                            {.level=100, .pitch={.fixed=true, .coarse=9, .fine=29}, .detune=50, .attack=0, .decay=0, .sustain=100, .loop=false}, 
+                            {.level=25, .pitch={.fixed=true, .coarse=10, .fine=93}, .detune=50, .attack=0, .decay=55, .sustain=75, .loop=false}, 
+                            {.level=50, .pitch={.fixed=false, .coarse=0, .fine=50}, .detune=50, .attack=50, .decay=0, .sustain=100, .loop=false}, 
                             {.level=0, .pitch={.fixed=false, .coarse=1, .fine=0}, .detune=50, .attack=0, .decay=0, .sustain=100, .loop=false}
                         }
                     }, {
